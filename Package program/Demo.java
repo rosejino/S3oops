166203eb8da391254c2277e2b55b7317880466be
Rose Jino <rosejino2020@gmail.com>
2025-08-11 22:06:51 +0530

    package

---
+++ Package program/Demo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo {
+    public String title = "Example for package";
+
+    public void printSummary() {
+        System.out.println("This Demo class is from the 'demo' package and is accessed by the Main class.");
+    }
+}
---
+++ Package program/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import demo.Demo;
+
+public class Main {
+    public static void main(String[] args) {
+        Demo example = new Demo();
+
+      System.out.println(example.title);
+        example.printSummary();
+    }
+}
